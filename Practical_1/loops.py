efb4c5ef487ad18a03dd9b8d1be76683032a0863
JakeWade01 <115449165+JakeWade01@users.noreply.github.com>
2022-11-09 20:45:33 +1000

    Files Required

---
+++ Practical_1/loops.py
@@ -0,0 +1,25 @@
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+for j in range(0, 100, 10):
+    print(j, end=' ')
+print()
+
+for k in range(20, 1, -1):
+    print(k, end=' ')
+print()
+print()
+
+no_of_stars = int(input("How Many Stars?: "))
+
+print("Number of Stars:", no_of_stars)
+for l in range(0, no_of_stars, 1):
+    print("*", end='')
+print()
+
+for m in range(0, no_of_stars, 1):
+    stars_counter = no_of_stars - (no_of_stars - m)
+    for o in range(0, stars_counter + 1, 1):
+        print("*", end='')
+    print()
---
+++ Practical_1/menus.py
@@ -0,0 +1,19 @@
+
+name = input("What is your name: ")
+
+MENU = """
+H(ello)
+G(oodbye)
+Q(uit)
+"""
+
+print(MENU)
+choice = input(">>>").upper()
+while choice != "Q":
+    if choice == "H":
+        print("Hello", name)
+    elif choice == "G":
+        print("Goodbye", name)
+    else:
+        print("Invalid Option")
+    choice = input(">>>").upper()
