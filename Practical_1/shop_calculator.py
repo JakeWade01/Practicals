d1dda294d7c2c4aae235c7565c6f945ed04fbe30
Jake <Jake.wade@my.jcu.edu.au>
2022-11-09 20:43:52 +1000

    Required Files

---
+++ Practical_1/shop_calculator.py
@@ -0,0 +1,17 @@
+
+
+no_of_items = int(input("How many items: "))
+
+while no_of_items <= 0:
+    print("Invalid Number of Items")
+    no_of_items = int(input("How many items: "))
+
+total_price = 0
+for i in range(0, no_of_items, 1):
+    single_price = float(input("Price of item: "))
+    total_price = total_price + single_price
+
+if total_price > 100:
+    total_price = total_price * 0.9
+
+print("Total price for", no_of_items, "items is", f"${total_price:,.2f}")
